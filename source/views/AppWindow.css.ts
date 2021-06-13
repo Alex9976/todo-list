a3e99814d4adbeeceddcc070e7e5bd8685f5a882
Alexander <60441130+Alex9976@users.noreply.github.com>
2021-06-13 13:46:52 +0300

    Revert "Merge branch 'master' into master"
    This reverts commit 17d3566a18e0a9438ed7442e4ade57663329d8da, reversing
    changes made to 5ee9818427371e088077a5f6931c32d26a14f7da.

--- source/views/AppWindow.css.ts
+++ source/views/AppWindow.css.ts
@@ -7,9 +7,10 @@ export const style = restyler(() => {
     AppWindow: css`
       font-size: calc(14px + (20 - 14) * (100vw - 800px) / (1920 - 800));
       font-family: Calibri, Tahoma, Arial, sans-serif;
+      background: url("../assets/background.jpg");
       background-size: cover;
       color: ${themes.active.foreground};
-      height: 100vh;
+      min-height: 100vh;
 
       a {
         text-decoration: none;
--- source/views/AppWindow.view.ts
+++ source/views/AppWindow.view.ts
@@ -3,33 +3,16 @@ import { App } from '../models/App'
 import { style } from './AppWindow.css'
 import { HomePageView } from './HomePage.view'
 
-const totalImage = 8
 
 export function AppWindow(
   app: App) {
   return (
     Div('AppWindow', e => {
       e.className = style.class.AppWindow
-      e.style.backgroundImage = 'url(../assets/'+(Math.floor(Math.random( ) * totalImage) + 1 )+'.jpg)'
-      //const crt = GetImg(e) //async api
-      RxDiv('Body', null, async e => {
+      RxDiv('Body', null, e => {
         e.className = style.class.Body
         HomePageView(app)
       })
     })
   )
-}
-
-async function GetImg(e: HTMLDivElement)
-{
-  const url = 'https://api.unsplash.com/photos/random?query=nature&orientation=landscape&client_id=wJQ8LjVThYrSF8a9uPXYnEE68op_IBc5MuY5cRZci7w'
-  const res = await fetch(url)
-  if (res.ok) {
-    const data = await res.json()
-    e.style.backgroundImage = `url(${data.urls.regular})`
-  }
-  else
-  {
-    e.style.backgroundImage = 'url(../assets/background.jpg)'
-  }
 }
